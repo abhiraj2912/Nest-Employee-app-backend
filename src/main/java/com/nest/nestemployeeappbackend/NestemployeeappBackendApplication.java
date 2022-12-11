0c98063685060e135c60a7620322ca39c68d74b1
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-11 21:32:31 +0530

    first

---
+++ src/main/java/com/nest/nestemployeeappbackend/NestemployeeappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestemployeeappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestemployeeappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestemployeeappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/nestemployeeappbackend/NestemployeeappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.nestemployeeappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestemployeeappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

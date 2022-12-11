92240bc4499a85a72816344158bf7c67afbc69e0
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-11 22:15:13 +0530

    model

---
+++ src/main/java/com/nest/nestemployeeappbackend/model/Task.java
@@ -0,0 +1,60 @@
+package com.nest.nestemployeeappbackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String taskName;
+    private String description;
+    private String status;
+
+    public Task() {
+    }
+
+    public Task(int id, String taskName, String description, String status) {
+        this.id = id;
+        this.taskName = taskName;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
